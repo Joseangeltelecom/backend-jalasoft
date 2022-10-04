d187526f0cbd43e71435bb267a5811d27cdd16c0
jose angel <joseangeltelecom@gmail.com>
2022-10-04 18:11:05 -0500

    Adding Liskov Exercise.

---
+++ Liskov/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liskov
+{
+    public class CuentaBancaria
+    {
+        private double _saldo;
+        private static ulong _numeroDeIdentificador = 1;
+        private ulong _identificador;
+
+        public ulong Identificador { get { return _identificador; } }
+        public double Saldo { get { return _saldo; } }
+        public CuentaBancaria(double saldoInicial)
+        {
+            _identificador = _numeroDeIdentificador++;
+            _saldo = saldoInicial;
+        }
+
+        public void TranseferirMonto(CuentaBancaria cuentaDestino, double monto)
+        {
+            this._saldo -= monto;
+            cuentaDestino._saldo += monto;
+        }
+    }
+
+    // parent
+    public abstract class EstadoDeCuenta
+    {
+        protected CuentaBancaria _cuenta;
+        public virtual string GenerarContenido()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Estado de cuenta\n");
+            stringBuilder.Append("\tEstado de cuenta");
+            stringBuilder.AppendLine(_cuenta.Identificador.ToString());
+            stringBuilder.Append("\tSaldo:");
+            stringBuilder.AppendFormat("{0:f2}", _cuenta.Saldo);
+            return stringBuilder.ToString();
+        }
+        public abstract string GenerarEncabezado();
+    }
+
+    public abstract class EstadoDeCuentaConPie : EstadoDeCuenta
+    {
+        public abstract string GenerarPie();
+    }
+
+    // Child A:
+    public class FormatoADeEstadoDeCuenta : EstadoDeCuentaConPie
+    {
+        public FormatoADeEstadoDeCuenta(CuentaBancaria cuenta)
+        {
+            this._cuenta = cuenta;
+        }
+        public override string GenerarEncabezado()
+        {
+            return "----------FORMATO A-----------\n";
+        }
+        public override string GenerarPie()
+        {
+            return "----------PIE DE FORMATO A-----------\n";
+        }
+    }
+
+    // child B:
+    public class FormatoBDeEstadoDeCuenta : EstadoDeCuenta
+    {
+        public FormatoBDeEstadoDeCuenta(CuentaBancaria cuenta)
+        {
+            this._cuenta = cuenta;
+        }
+        override public string GenerarEncabezado()
+        {
+            return "----------FORMATO B-----------\n";
+        }
+    }
+    class Program
+    {
+        static void Imprimir(List<EstadoDeCuenta> estadosDeCuenta)
+        {
+            foreach (EstadoDeCuenta estadoDeCuenta in estadosDeCuenta)
+            {
+                Console.WriteLine(estadoDeCuenta.GenerarEncabezado());
+                Console.WriteLine(estadoDeCuenta.GenerarContenido());
+                if (estadoDeCuenta is EstadoDeCuentaConPie)
+                {
+                    FormatoADeEstadoDeCuenta formatoADeEstadoDeCuenta = (FormatoADeEstadoDeCuenta)estadoDeCuenta;
+                    Console.WriteLine(formatoADeEstadoDeCuenta.GenerarPie());
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            CuentaBancaria cuenta1 = new CuentaBancaria(100);
+            CuentaBancaria cuenta2 = new CuentaBancaria(150);
+
+            cuenta2.TranseferirMonto(cuenta1, 30);
+            List<EstadoDeCuenta> estadosDeCuenta = new List<EstadoDeCuenta>
+            {
+                new FormatoADeEstadoDeCuenta(cuenta1),
+                new FormatoADeEstadoDeCuenta(cuenta2),
+                new FormatoBDeEstadoDeCuenta(cuenta1),
+                new FormatoBDeEstadoDeCuenta(cuenta2)
+            };
+            Imprimir(estadosDeCuenta);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Liskov/README.md
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class CuentaBancaria
+    {
+        private double _saldo;
+        private static ulong _numeroDeIdentificador = 1;
+        private ulong _identificador;
+
+        public ulong Identificador { get { return _identificador; } }
+        public double Saldo { get { return _saldo; } }
+        public CuentaBancaria(double saldoInicial)
+        {
+            _identificador = _numeroDeIdentificador++;
+            _saldo = saldoInicial;
+        }
+
+        public void TranseferirMonto(CuentaBancaria cuentaDestino, double monto)
+        {
+            this._saldo -= monto;
+            cuentaDestino._saldo += monto;
+        }
+    }
+
+    public interface IEstadoDeCuenta
+    {
+        public string GenerarContenido();
+        public string GenerarEncabezado();
+        public string GenerarPie();
+    }
+
+    public abstract class EstadoDeCuenta
+    {
+        protected CuentaBancaria _cuenta;
+        public virtual string GenerarContenido()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Estado de cuenta\n");
+            stringBuilder.Append("\tEstado de cuenta");
+            stringBuilder.AppendLine(_cuenta.Identificador.ToString());
+            stringBuilder.Append("\tSaldo:");
+            stringBuilder.AppendFormat("{0:f2}", _cuenta.Saldo);
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class FormatoADeEstadoDeCuenta : EstadoDeCuenta, IEstadoDeCuenta
+    {
+        public FormatoADeEstadoDeCuenta(CuentaBancaria cuenta)
+        {
+            this._cuenta = cuenta;
+        }
+        public string GenerarEncabezado()
+        {
+            return "----------FORMATO A-----------\n";
+        }
+        public string GenerarPie()
+        {
+            return "----------PIE DE FORMATO A-----------\n";
+        }
+    }
+    public class FormatoBDeEstadoDeCuenta : EstadoDeCuenta, IEstadoDeCuenta
+    {
+        public FormatoBDeEstadoDeCuenta(CuentaBancaria cuenta)
+        {
+            this._cuenta = cuenta;
+        }
+        public string GenerarEncabezado()
+        {
+            return "----------FORMATO B-----------\n";
+        }
+        public string GenerarPie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Imprimir(IList<IEstadoDeCuenta> estadosDeCuenta)
+        {
+            foreach(IEstadoDeCuenta estadoDeCuenta in estadosDeCuenta)
+            {
+                Console.WriteLine(estadoDeCuenta.GenerarEncabezado());
+                Console.WriteLine(estadoDeCuenta.GenerarContenido());
+                if(estadoDeCuenta is FormatoADeEstadoDeCuenta)
+                {
+                    Console.WriteLine(estadoDeCuenta.GenerarPie());
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            CuentaBancaria cuenta1 = new CuentaBancaria(100);
+            CuentaBancaria cuenta2 = new CuentaBancaria(150);
+            cuenta2.TranseferirMonto(cuenta1, 30);
+            IList<IEstadoDeCuenta> estadosDeCuenta = new List<IEstadoDeCuenta>
+            {
+                new FormatoADeEstadoDeCuenta(cuenta1),
+                new FormatoADeEstadoDeCuenta(cuenta2),
+                new FormatoBDeEstadoDeCuenta(cuenta1),
+                new FormatoBDeEstadoDeCuenta(cuenta2)
+            };
+            Imprimir(estadosDeCuenta);
+            Console.ReadLine();
+        }
+    }
+}
